bd69233c882ce14d6e06efca399c0663d73af94e
sivachandanc <sivachandan1996@gmail.com>
2025-10-15 21:41:12 -0500

    Add Aqua Dreams temperature reader and MQTT client implementation; update deployment script to include all Python files

---
+++ mqtt_client.py
@@ -0,0 +1,26 @@
+from awscrt import mqtt
+from awsiot import mqtt_connection_builder
+import os
+
+def mqtt_client():
+    try:
+        CERT_DIR = "/home/sivachandan/aws_iot"
+        CA   = os.path.join(CERT_DIR, "AmazonRootCA1.pem")
+        CERT = os.path.join(CERT_DIR, "5ddb201d4ac939508f608bc9d497e59eeb18c5741f08945a1dbc6ed8c50afc87-certificate.pem.crt")
+        KEY  = os.path.join(CERT_DIR, "5ddb201d4ac939508f608bc9d497e59eeb18c5741f08945a1dbc6ed8c50afc87-private.pem.key")
+
+        mqtt_connection = mqtt_connection_builder.mtls_from_path(
+            endpoint="ar1b3cylu99rl-ats.iot.us-east-1.amazonaws.com",
+            cert_filepath=CERT,
+            pri_key_filepath=KEY,
+            ca_filepath=CA,
+            client_id="pi-aqua-dreams",
+            keep_alive_secs=60
+        )
+
+        mqtt_connection.connect().result()
+        print("✅ Connected to AWS IoT Core")
+        return mqtt_connection
+    except Exception as e:
+        print(f"❌ Error connecting to AWS IoT Core: {e}")
+        return None
