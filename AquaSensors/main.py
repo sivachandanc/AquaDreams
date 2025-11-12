f4926d8739155bc7a499537f0fd86f70f2c29feb
sivachandanc <sivachandan1996@gmail.com>
2025-11-11 19:59:32 -0600

    Implement command-line argument parsing in main script to control temperature sensor reading mode

--- AquaSensors/main.py
+++ AquaSensors/main.py
@@ -1,6 +1,14 @@
+import argparse
+
 def main():
-    print("Hello from aquadreams!")
+    parser = argparse.ArgumentParser(description="Aqua Dreams IoT project")
+    parser.add_argument('-t', '--temp_sensor', default=False, type=str, help='Read temperature sensor') 
+    args = parser.parse_args()
+    if args.temp_sensor == "true":
+        print("Reading temperature sensor")
+    else:
+        print("Running main mode")
 
 
 if __name__ == "__main__":
-    main()
+    pass
