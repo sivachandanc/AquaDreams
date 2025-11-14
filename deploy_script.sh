eac779c28fc3470bc6a3a1e940ef9ee2661490b0
sivachandanc <sivachandan1996@gmail.com>
2025-11-13 19:19:00 -0600

    Refactor deploy script to transfer specific wheel file and remove obsolete MQTT test script. Update main module with detailed documentation for command-line interface and usage examples.

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scp -r /Users/sivachandan/PersonalProjects/AquaDreams/AquaSensors/* sivachandan@100.69.89.78:~/AquaSensors/
+scp /Users/sivachandan/PersonalProjects/AquaDreams/dist/aquadreams-0.1.0-py3-none-any.whl sivachandan@100.69.89.78:~/AquaSensors/
